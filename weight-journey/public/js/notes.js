1163669bd8d62e03cc519242941784bb99c0cbf8
Janet <jflores49@neiu.edu>
2020-09-23 20:48:55 -0500

    package

--- weight-journey/public/js/notes.js
+++ weight-journey/public/js/notes.js
@@ -1,7 +1,12 @@
 let graph = require('./graph')
+let moment = require('moment')
 
 exports.notesDescription = function () {
     graph.graphDescription()
     console.log("Users can write diary entries.")
+    let date = "12/15/2020"
+    let m = moment(date, "MM/DD/YYYY").format("MMM Do YYYY")
+    console.log("Using the moment package, I can change the date format from " + date + " to " + m.toString() + ".");
+
 }
 
