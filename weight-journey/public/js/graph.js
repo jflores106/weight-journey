2cc9b9dfec0990a6f87830afa05ecd1aba58a581
Janet <jflores49@neiu.edu>
2020-09-30 21:04:49 -0500

    events

--- weight-journey/public/js/graph.js
+++ weight-journey/public/js/graph.js
@@ -1,8 +1,16 @@
 let description = require('./description')
 
-exports.graphDescription = function () {
+
+exports.graphDescription = function (emitter) {
     description.getDescription()
     console.log("A graph will display users' weight entries over a period.")
+
+
+    emitter.on('my event', function (i){
+        console.log('Event received ' + i)
+
+    })
+
 }
 
 
